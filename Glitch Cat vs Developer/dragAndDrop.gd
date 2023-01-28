346b109e6803852209d322246371f32e097e5aec
kerchup <94553887+kerchup@users.noreply.github.com>
2023-01-28 17:58:50 -0600

    made wall collision

--- Glitch Cat vs Developer/dragAndDrop.gd
+++ Glitch Cat vs Developer/dragAndDrop.gd
@@ -2,12 +2,27 @@ extends KinematicBody2D
 
 var can_grab = false
 var grabbed_offset = Vector2()
+export var horizontal = true
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
-		can_grab = event.pressed
-		grabbed_offset = position - get_global_mouse_position()
+		if event.is_action_released("click"):
+			can_grab = false
+		else:
+			can_grab = true
+		if horizontal:
+			grabbed_offset = position.x - get_global_mouse_position().x
+		else:
+			grabbed_offset = position.y - get_global_mouse_position().y
+	
 		
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_grab:
-		position = get_global_mouse_position() + grabbed_offset
+		if horizontal:
+			var x = get_global_mouse_position().x + grabbed_offset
+			var width = self.get_node("CollisionShape2D").shape.extents.x
+			position.x = max(min(x, 1820-width), 650+width)
+		else:
+			var y = get_global_mouse_position().y + grabbed_offset
+			var height = self.get_node("CollisionShape2D").shape.extents.y
+			position.y = max(min(y, 980-height), 100+height)
