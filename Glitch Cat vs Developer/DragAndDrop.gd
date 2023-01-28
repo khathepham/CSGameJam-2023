a352572918cb60a183f122312bdde3dfb2ff069d
kerchup <94553887+kerchup@users.noreply.github.com>
2023-01-28 16:35:52 -0600

    Working on dragging

--- Glitch Cat vs Developer/DragAndDrop.gd
+++ Glitch Cat vs Developer/DragAndDrop.gd
@@ -1,16 +1,15 @@
-extends Node
+extends KinematicBody2D
+
+var can_grab = false
+var grabbed_offset = Vector2()
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		can_grab = event.pressed
+		grabbed_offset = position - get_global_mouse_position()
+		
+func _process(delta):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_grab:
+		position = get_global_mouse_position() + grabbed_offset
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
