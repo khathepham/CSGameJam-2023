95cc57762fcb46c35236a80f9f063adc24b82ef0
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2023-01-29 16:22:18 -0600

    Add minigame nodes
    Added minigame nodes on the map. Player must walk over them to interact. To interact, press E

---
+++ Glitch Cat vs Developer/Minigame.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+export var scene : PackedScene
+signal complete
+
+var complete = false
+var interactable = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MinigameSprite.texture = load("res://assets/node.png")
+	modulate = Color(1,1,1)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	print(interactable)
+	if interactable && Input.is_action_just_pressed("interact"):
+		print("play")
+		get_tree().change_scene_to(scene)
+
+func _on_Minigame_body_entered(_body):
+	interactable = true
+	print("interactable")
+	if !complete:
+		modulate = Color(1.5,1.5,1.5)
+
+
+func _on_Minigame_body_exited(_body):
+	interactable = false
+	print("exit")
+	modulate = Color(1,1,1)
+	if !complete:
+		emit_signal("complete")
+	
+func _on_Minigame_complete():
+	$MinigameSprite.texture = load("res://assets/frayed node.png")
+	complete = true
--- Glitch Cat vs Developer/Player/Player.gd
+++ Glitch Cat vs Developer/Player/Player.gd
@@ -1,9 +1,8 @@
 extends KinematicBody2D
-signal hit
 
 
 # Declare member variables here. Examples:
-export var speed = 800 # How fast the player will move (pixels/sec).
+export var speed = 1200 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 onready var _animated_sprite = $AnimatedSprite
 
