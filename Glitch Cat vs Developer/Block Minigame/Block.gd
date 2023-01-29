69c821471bb256d8c1fc71b23c5e9f46e4cbd811
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2023-01-28 19:55:29 -0600

    Add block collisions
    Blocks are now Area2D nodes instead of KineticBody2D, although this may need to change later. Upon colliding, blocks are no longer grabbed, and can't move. Player must click on the block again to redrag.

--- Glitch Cat vs Developer/Block Minigame/Block.gd
+++ Glitch Cat vs Developer/Block Minigame/Block.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Area2D
 
 var can_grab = false
 var is_dragging = false;
@@ -13,33 +13,34 @@ func _input_event(viewport, event, shape_idx):
 				grabbed_offset = position.x - get_global_mouse_position().x
 			else:
 				grabbed_offset = position.y - get_global_mouse_position().y
-				
-	
-		
+
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_grab:
 		if horizontal:
 			var x = get_global_mouse_position().x + grabbed_offset
-			var width = self.get_node("CollisionShape2D").shape.extents.x
+			var width = self.get_node("Block_Hitbox").shape.extents.x
 			position.x = max(min(x, 1820-width), 650+width)
 		else:
 			var y = get_global_mouse_position().y + grabbed_offset
-			var height = self.get_node("CollisionShape2D").shape.extents.y
+			var height = self.get_node("Block_Hitbox").shape.extents.y
 			position.y = max(min(y, 980-height), 100+height)
 	if (!Input.is_mouse_button_pressed(BUTTON_LEFT)):
 		can_grab = false
 			
+func _stop_move():
+	can_grab = false
+		
+
 func _check_mouse_overlap():
 	var mouseX = get_global_mouse_position().x
 	var mouseY = get_global_mouse_position().y
-	var width = self.get_node("CollisionShape2D").shape.extents.x
-	var height = self.get_node("CollisionShape2D").shape.extents.y
+	var width = self.get_node("Block_Hitbox").shape.extents.x
+	var height = self.get_node("Block_Hitbox").shape.extents.y
 	var insideX = mouseX > position.x-width && mouseX < position.x+width
 	var insideY = mouseY > position.y-height && mouseY < position.y+height
 	if insideX && insideY:
 		return true
 	return false
 
-
-func _on_CenterBlock_area_entered(area):
-	
+func _on_Block_area_entered(area):
+	area._stop_move()
