4453c4a95d5fa65b8b2e0b761d84a3bdab6ecaef
Ahmed <90289396+ahmedshawqir@users.noreply.github.com>
2023-01-29 10:58:44 -0600

    finished animating ???

--- Glitch Cat vs Developer/Map.gd
+++ Glitch Cat vs Developer/Map.gd
@@ -5,7 +5,7 @@ signal hit
 # Declare member variables here. Examples:
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
-
+onready var _animated_sprite = $AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,12 +16,16 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
+		_animated_sprite.play("right")
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
+		_animated_sprite.play("left")
 		velocity.x -= 1
 	if Input.is_action_pressed("move_down"):
+		_animated_sprite.play("down")
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
+		_animated_sprite.play("up")
 		velocity.y -= 1
 
 	if velocity.length() > 0:
