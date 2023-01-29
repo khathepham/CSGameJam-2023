702646548c2ae3adcd1374ef73cfe3bc936a5118
arasantos <100260167+arasantos@users.noreply.github.com>
2023-01-29 14:14:06 -0600

    added a level and everything

---
+++ Glitch Cat vs Developer/redZoneCollisions.gd
@@ -0,0 +1,39 @@
+# for the red zone !!
+
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+#delays next scene then goes to gameover scene, try again
+func _on_redZone_mouse_entered():
+	yield(get_tree().create_timer(0.5), "timeout") #delays next scene
+	
+	get_tree().change_scene("res://CursorGameOver.tscn")
+
+
+#the rest is the same collision but in different areas...
+func _on_redZone2_mouse_entered():
+	_on_redZone_mouse_entered()
+
+
+func _on_redZone3_mouse_entered():
+	_on_redZone_mouse_entered()
+
+
+func _on_redZone4_mouse_entered():
+	_on_redZone_mouse_entered()
+
+
+func _on_redZone5_mouse_entered():
+	_on_redZone_mouse_entered()
+
+
+func _on_redZone6_mouse_entered():
+	_on_redZone_mouse_entered()
+
+
+func _on_redZone7_mouse_entered():
+	_on_redZone_mouse_entered()
