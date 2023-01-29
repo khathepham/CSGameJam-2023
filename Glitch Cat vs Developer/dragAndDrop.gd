9a8ffb4b9ddadd09b68c922db99e061200a84a01
kerchup <94553887+kerchup@users.noreply.github.com>
2023-01-28 18:55:29 -0600

    FIXED DRAGGING!!!!!!!!!!!!!!!

--- Glitch Cat vs Developer/dragAndDrop.gd
+++ Glitch Cat vs Developer/dragAndDrop.gd
@@ -1,19 +1,19 @@
 extends KinematicBody2D
 
 var can_grab = false
+var is_dragging = false;
 var grabbed_offset = Vector2()
 export var horizontal = true
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
-		if event.is_action_released("click"):
-			can_grab = false
-		else:
+		if event.pressed && _check_mouse_overlap():
 			can_grab = true
-		if horizontal:
-			grabbed_offset = position.x - get_global_mouse_position().x
-		else:
-			grabbed_offset = position.y - get_global_mouse_position().y
+			if horizontal:
+				grabbed_offset = position.x - get_global_mouse_position().x
+			else:
+				grabbed_offset = position.y - get_global_mouse_position().y
+				
 	
 		
 func _process(delta):
@@ -26,3 +26,16 @@ func _process(delta):
 			var y = get_global_mouse_position().y + grabbed_offset
 			var height = self.get_node("CollisionShape2D").shape.extents.y
 			position.y = max(min(y, 980-height), 100+height)
+	if (!Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		can_grab = false
+			
+func _check_mouse_overlap():
+	var mouseX = get_global_mouse_position().x
+	var mouseY = get_global_mouse_position().y
+	var width = self.get_node("CollisionShape2D").shape.extents.x
+	var height = self.get_node("CollisionShape2D").shape.extents.y
+	var insideX = mouseX > position.x-width && mouseX < position.x+width
+	var insideY = mouseY > position.y-height && mouseY < position.y+height
+	if insideX && insideY:
+		return true
+	return false
