08c0776b06407883988a7d9efa69945862aab05b
Jared-Rost <92229614+Jared-Rost@users.noreply.github.com>
2023-01-28 21:49:39 -0600

    Added additional cutscenes

---
+++ First cut-scene/Timer4.gd
@@ -0,0 +1,23 @@
+extends Timer
+
+
+var timer = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(2)
+	timer.connect("timeout", self, "_on_Timer_timeout")
+	add_child(timer)
+	timer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://Roombackground5.tscn")
